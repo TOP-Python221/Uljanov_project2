60dd153bf393a392751119a4cc5903e24197866e
UlyanovSergey <108969866+UlyanovSergey@users.noreply.github.com>
2022-11-29 22:01:28 +0300

    добавил классы

--- index.py
+++ index.py
@@ -1,23 +1,75 @@
 from datetime import datetime as dt
+import dataclasses as dataclasses
+
+
+
+class Body:
+    helth: int
+    stamina: int
+    hunger: int
+    thirst: int
+
+    def tick_changes(self):
+        pass
+
 
 
 class Creature:
     """ класс существа """
     def __int__(self, name: str,
                 birthday: dt,
-                body: 'Body'):
-        self.name = name
-        self.body = body
-
-
-
-class Body:
-    def __int__(self,
-                helth: int,
+                helts: int,
                 stamina: int,
                 hunger: int,
-                thirst: int):
-        self.helts = helth
+                thirst: int,
+                ):
+        self.name = name
+        self.helts = helts
         self.stamina = stamina
         self.hunger = hunger
         self.thirst = thirst
+
+    def feed(self):
+        pass
+
+    def play(self):
+        pass
+
+    def talk(self):
+        pass
+
+class Ming:
+    pass
+
+class StatesCalculator:
+
+    def new_body(self) -> Body:
+        pass
+
+    def new_mind(self):
+        pass
+
+    def new_creature(self) -> Creature:
+        pass
+
+class PersistenceManager:
+
+    def read_file(self):
+        pass
+
+    def write_file(self):
+        pass
+
+
+class StatesManager:
+    pass
+
+class BodyState:
+    pass
+
+class Mind:
+   pass
+
+class MindState:
+    pass
+
